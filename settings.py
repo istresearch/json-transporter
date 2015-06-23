50509932c67494497f71b36a059ddc49bafab291
Jason Haas <jason.haas@istresearch.com>
2015-06-23 01:33:53 -0400

    use localsettings as well as settings

--- settings.py
+++ settings.py
@@ -2,21 +2,33 @@ JSON_SETTINGS = {
 
 }
 
-ES_SETTINGS ={
-
+ES_SETTINGS = {
+	'host': 'localhost:9200',
+    'ssl': 'false'
 }
 
 S3_SETTINGS = {
-
+	'access_key': 'use_localsettings',
+	'secret_key': 'use_localsettings'
 }
 
 MONGO_SETTINGS = {
-    
+
 }
 
 HBASE_SETTINGS = {
-    
+	
 }
+
 KAFKA_SETTINGS = {
-    
+	'host': 'localhost:9092',
+	'prefix': 'dingo'
 }
+
+# Local Overrides
+# ~~~~~~~~~~~~~~~
+
+try:
+    from localsettings import *
+except ImportError:
+    pass
