fe5a377c6230831cbacc68166026167a3dfc6c53
Jason Haas <jason.haas@istresearch.com>
2015-08-28 15:00:32 -0400

    more setup.py hacking

--- setup.py
+++ setup.py
@@ -1,31 +1,61 @@
-from setuptools import find_packages
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import sys
+from setuptools import setup, find_packages
+
+
+def readme():
+    ''' Returns README contents as str '''
+    with open('README.md') as f:
+        return f.read()
+
+install_requires = [
+    'urllib3',
+    'elasticsearch>=1.6',
+    'boto',
+    'pymongo',
+    'happybase',
+    'pykafka',
+    'docopt'
+]
+
+lint_requires = [
+    'pep8',
+    'pyflakes'
+]
+
+tests_require = ['nose']
+dependency_links = []
+setup_requires = []
+extras_require={
+    'test': tests_require,
+    'all': install_requires + tests_require,
+    'docs': ['sphinx'] + tests_require,
+    'lint': lint_requires
+}
+
+if 'nosetests' in sys.argv[1:]:
+    setup_requires.append('nose')
 
 setup(
     name='json-transporter',
-    packages=find_packages(),
-    package_data={},
-    install_requires=[
-        'urllib3',
-        'elasticsearch>=1.6',
-        'boto',
-        'pymongo',
-        'happybase',
-        'pykafka',
-        'docopt'
-    ],
-    entry_points={'console_scripts': ['tport=tport.tport:main']},
-    version='0.5',
+    version='0.1',
     description='A JSON data transporter',
+    long_description=readme(),
     author='Jason Haas',
     author_email='jasonrhaas@gmail.com',
     license='MIT',
     url='https://github.com/istresearch/json-transporter',
-    # download_url='https://github.com/istresearch/json-transporter/tarball/0.1',
-    keywords='json elasticsearch s3 kafka mongo',
+    keywords=['json', 'elasticsearch', 's3', 'kafka', 'mongo'],
+    packages=find_packages(),
+    package_data={},
+    install_requires=install_requires,
+    tests_require=tests_require,
+    setup_requires=setup_requires,
+    extras_require=extras_require,
+    dependency_links=dependency_links,
+    zip_safe=True,
+    test_suite='nose.collector',
+    include_package_data=True,
+    entry_points={'console_scripts': ['tport=transporter.tport:main']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
